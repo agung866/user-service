d107a13f9cb7375ffa166ddf5e6d30982bb1364f
agung866 <setiawan.agung866@hmail.com>
2025-05-20 11:16:45 +0700

    implementation reactive programing

--- src/main/java/com/example/user_service/service/ProductService.java
+++ src/main/java/com/example/user_service/service/ProductService.java
@@ -12,6 +12,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -21,34 +22,36 @@ import java.util.Date;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public void addProduct(ProductRequest request) {
+    public Mono<Product> addProduct(ProductRequest request) {
         var product = new Product()
                 .setName(request.getName())
                 .setDescription(request.getDescription())
                 .setPrice(request.getPrice())
                 .setCreatedAt(new Date());
-        productRepository.save(product);
+        return productRepository.save(product);
     }
 
-    public void deleteProduct(Integer id) {
-        productRepository.deleteById(id);
+    public Mono<Void> deleteProduct(Integer id) {
+        return productRepository.deleteById(id);
     }
 
 
-    public ResponseEntity<?> updateProduct(Integer id, UpdateProductRequest updateProductRequest) {
-        var getProduct = productRepository.findById(id.longValue())
-                .orElseThrow(() -> new DataNotFoundException("Data Product tidak ditemukan"));
+    public Mono<Product> updateProduct(Integer id, UpdateProductRequest updateProductRequest) {
+        return productRepository.findById(id.longValue()).flatMap(p->{
+                    if (!StringUtils.isBlank(updateProductRequest.getName())) {
+                        p.setName(updateProductRequest.getName());
+                    }
+                    if (!StringUtils.isBlank(updateProductRequest.getDescription())) {
+                        p.setDescription(updateProductRequest.getDescription());
+                    }
+                    if (updateProductRequest.getPrice() != null) {
+                        p.setPrice(updateProductRequest.getPrice());
+                    }
+                    return productRepository.save(p);
 
-        if (!StringUtils.isBlank(updateProductRequest.getName())) {
-            getProduct.setName(updateProductRequest.getName());
-        }
-        if (!StringUtils.isBlank(updateProductRequest.getDescription())) {
-            getProduct.setDescription(updateProductRequest.getDescription());
-        }
-        if (updateProductRequest.getPrice() != null) {
-            getProduct.setPrice(updateProductRequest.getPrice());
-        }
-        productRepository.save(getProduct);
-        return ResponseEntity.ok(getProduct);
+                })
+                .switchIfEmpty(Mono.error( new DataNotFoundException("Data Product tidak ditemukan")));
+
+
     }
 }
